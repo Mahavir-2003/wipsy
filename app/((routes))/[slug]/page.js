ee7d760f38eecc5c5afc1cbdbecfb739f4159a7c
Mahavir-2003 <patelmahavir2003@outlook.com>
2024-04-18 14:58:19 +0530

    Add ChatBox component to page.js and update styles along with file upload functionality

--- app/((routes))/[slug]/page.js
+++ app/((routes))/[slug]/page.js
@@ -1,11 +1,28 @@
 "use client";
 import ChatBox from "@/app/components/ChatBox";
+import UploadViewer from "@/app/components/UploadViewer";
 import { Spinner } from "@nextui-org/spinner";
-import Image from "next/image";
+import NextImage from "next/image";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const page = () => {
+
+  const [uploads, setUploads] = useState([
+    {
+      id: "1",
+      type: "image", // file
+      url : "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+      name: "image.jpg",
+    },
+    {
+      id: "2",
+      type: "file", // file
+      name: "file.pdf",
+    },
+  ]);
+
+  // Extras for the page
   const { slug } = useParams();
   const [ID, setID] = useState(slug);
   useEffect(() => {
@@ -19,20 +36,66 @@ const page = () => {
     }
   }, []);
 
+
+  // Logical part of the page
+  const fileUploadClickHandler = () => {
+    // open file upload dialog
+    const fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.accept = '*';
+    fileInput.multiple = true;
+    fileInput.click();
+    fileInput.addEventListener('change', (e) => {
+      const files = e.target.files;
+      if (files.length > 0) {
+        const newUploads = [...uploads];
+        for (let i = 0; i < files.length; i++) {
+          const file = files[i];
+          const newUpload = {
+            id: new Date().getTime().toString(),
+            type: file.type.split('/')[0],
+            name: file.name,
+            file: file,
+          };
+          if (newUpload.type === 'image') {
+            newUpload.url = URL.createObjectURL(file);
+          }
+          newUploads.push(newUpload);
+        }
+        setUploads(newUploads);
+      }
+    });
+  };
+
+  const insertClickHandler = () => {
+    console.log("insertClickHandler");
+  };
+
+  const textAreaDragAndDropHandler = () => {
+    console.log("textAreaDragAndDropHandler");
+  };
+
+
+
   return (
     <div className="w-full h-full flex justify-center items-end">
       <div className=" h-full w-[70%]  flex flex-col justify-start items-center">
         <div className=" w-full overflow-hidden py-3 flex-1 h-full">
           <ChatBox />
         </div>
-        <div className="Input-Area w-full flex justify-center items-center mb-5 max-h-fit">
-          <button className="file-upload-button group bg-transparent border-[1px] duration-200 hover:border-[#fafafa] border-[#fafafa]/15 rounded-md h-full aspect-square mr-2 flex justify-center items-center">
+        <div className="Input-Area w-full flex justify-center items-center mb-5 max-h-fit relative">
+          <div className={`absolute top-0 translate-y-[-100%] left-0 w-[40%] aspect-video pb-3 ${uploads.length == 0 ? "hidden" : "block"}`}>
+              <div id="uploads" className="uploads w-full h-full bg-transparant border-[1px] border-[#fafafa]/15 rounded-md overflow-hidden">
+                <UploadViewer uploads={uploads} setUploads={setUploads}/>
+              </div>
+          </div>
+          <button onClick={fileUploadClickHandler} className="file-upload-button group bg-transparent border-[1px] duration-200 hover:border-[#fafafa] border-[#fafafa]/15 rounded-md h-full aspect-square mr-2 flex justify-center items-center">
             {
               <div className="relative w-[50%] aspect-square flex justify-center items-center ">
                 {false ? (
                   <Spinner size="md" color="current" />
                 ) : (
-                  <Image
+                  <NextImage
                     src="/Upload.svg"
                     fill={true}
                     alt="send"
@@ -52,7 +115,7 @@ const page = () => {
                 {false ? (
                   <Spinner size="md" color="current" />
                 ) : (
-                  <Image
+                  <NextImage
                     src="/send.svg"
                     fill={true}
                     alt="send"
