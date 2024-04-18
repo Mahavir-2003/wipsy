06012f3a26475cdd23e99cabc61e7c75d63bce6e
Mahavir-2003 <patelmahavir2003@outlook.com>
2024-04-18 13:37:25 +0530

    Add ChatBox component to page.js along with upload button.

--- app/((routes))/[slug]/page.js
+++ app/((routes))/[slug]/page.js
@@ -1,4 +1,5 @@
 "use client";
+import ChatBox from "@/app/components/ChatBox";
 import { Spinner } from "@nextui-org/spinner";
 import Image from "next/image";
 import { useParams } from "next/navigation";
@@ -21,11 +22,11 @@ const page = () => {
   return (
     <div className="w-full h-full flex justify-center items-end">
       <div className=" h-full w-[70%]  flex flex-col justify-start items-center">
-        <div className="chat-history w-full flex-1 overflow-x-hidden overflow-y-scroll">
-          chatbox
+        <div className=" w-full overflow-hidden py-3 flex-1 h-full">
+          <ChatBox />
         </div>
-        <div className="Input-Area w-full flex justify-center items-center mb-10 max-h-fit">
-          <button className="group bg-transparent border-[1px] duration-200 hover:border-[#fafafa] border-[#fafafa]/15 rounded-md h-full aspect-square mr-2 flex justify-center items-center">
+        <div className="Input-Area w-full flex justify-center items-center mb-5 max-h-fit">
+          <button className="file-upload-button group bg-transparent border-[1px] duration-200 hover:border-[#fafafa] border-[#fafafa]/15 rounded-md h-full aspect-square mr-2 flex justify-center items-center">
             {
               <div className="relative w-[50%] aspect-square flex justify-center items-center ">
                 {false ? (
@@ -42,10 +43,10 @@ const page = () => {
             }
           </button>
           <textarea
-            className="w-[90%] h-[70px] placeholder:text-[#fafafa]/30 placeholder:font-light bg-transparent  resize-none border px-3 pt-3 rounded-md focus:outline-none focus:border-[#fafafa]/70 border-[#fafafa]/15"
+            className="text-area-input w-[90%] h-[70px] placeholder:text-[#fafafa]/30 placeholder:font-light bg-transparent  resize-none border px-3 pt-3 rounded-md focus:outline-none focus:border-[#fafafa]/70 border-[#fafafa]/15"
             placeholder="Type your message here"
           ></textarea>
-          <button className=" aspect-video flex justify-center items-center h-full hover:bg-yellow-300 bg-yellow-400 text-black text-lg font-medium tracking-wide rounded-md ml-2">
+          <button className="insert-button aspect-video flex justify-center items-center h-full hover:bg-yellow-300 bg-yellow-400 text-black text-lg font-medium tracking-wide rounded-md ml-2">
             {
               <div className="relative h-[50%] aspect-square flex justify-center items-center">
                 {false ? (
---
+++ app/components/ChatBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ChatBox = () => {
+  return (
+    <div className='bg-transparent min-h-full flex justify-center items-center chat-history w-full border-[1px] border-[#fafafa]/15 rounded-md px-3 py-2 overflow-x-hidden overflow-y-auto h-[200px]'>
+      
+      ChatBox<br/>
+    </div>
+  )
+}
+
+export default ChatBox
