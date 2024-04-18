3884fc230b2d4e68d0caf29731220a0c3898718a
Mahavir-2003 <patelmahavir2003@outlook.com>
2024-04-18 10:25:55 +0530

    Refactor page.js to handle dynamic slugs and redirect if necessary

--- app/((routes))/[slug]/page.js
+++ app/((routes))/[slug]/page.js
@@ -1,10 +1,21 @@
 "use client";
 import { useParams } from "next/navigation"
+import { useEffect, useState } from "react";
 
 const page = () => {
     const {slug} = useParams();
-  return (
-    <div className="w-full h-full flex justify-center items-center">{slug}</div>
+    const [ID, setID] = useState(slug);
+    useEffect(() => {
+      // remove the space(%20) and replace it with a dash(-)
+      const newSlug = decodeURIComponent(slug).replace(/\s+/g, '-').replace(/[^a-zA-Z0-9\-]/g, '');
+      if(newSlug != slug){ 
+        window.location.replace("/" + newSlug);
+        setID(newSlug);
+      }
+    }, [])
+
+  return (    
+    <div className="w-full h-full flex justify-center items-center">{ID}</div>
   )
 }
 
