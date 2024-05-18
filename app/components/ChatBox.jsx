d454e7c9aa21e6772f1a8448df900e67fc6df85d
Mahavir-2003 <patelmahavir2003@outlook.com>
2024-05-18 12:45:00 +0530

    Refactor ChatBox component to improve code readability and add syntax highlighting (Code Block Styling done)

--- app/components/ChatBox.jsx
+++ app/components/ChatBox.jsx
@@ -4,32 +4,36 @@ import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { nightOwl } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import CopyToClipboard from 'react-copy-to-clipboard';
-import { FaCopy } from 'react-icons/fa';
+import { GoCopy } from "react-icons/go";
 
 const customStyle = {
-  padding: '1rem',
   width : '100%',
-  borderRadius: '0.5rem',
   overflowX: 'auto',
   whiteSpace: 'pre-wrap',
   wordWrap: 'break-word',
   fontSize: '1rem',
   lineHeight: '1.5rem',
-  backgroundColor: '#151515BC',
-  border : '1px solid #FAFAFA2E',
+  backgroundColor: 'transparent',
 }
 
 const components = {
   code({node, inline, className, children, ...props}) {
     const match = /language-(\w+)/.exec(className || '')
     return !inline && match ? (
-      <div className="relative w-full">
-         <CopyToClipboard text={String(children).replace(/\n$/, '')}>
-          <button className="absolute right-2 top-2 bg-gray-800 text-white p-1 rounded-md">
-            <FaCopy />
+      <div className="relative w-full bg-[#1515156e] rounded-md border border-[#fafafa]/15 overflow-hidden">
+        <div className='w-full flex justify-between items-center py-2 px-4 border-b border-[#fafafa]/15'>
+          <p>{match[1]}</p>
+         <CopyToClipboard onCopy={
+          () => alert('Code Copied')
+         } text={String(children).replace(/\n$/, '')}>
+          <button>
+          <GoCopy />
           </button>
         </CopyToClipboard>
-        <SyntaxHighlighter language={match[1]} style={nightOwl} customStyle={customStyle} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} />
+        </div>
+        <div className=' w-full px-2'>
+        <SyntaxHighlighter className="code-block" language={match[1]} style={nightOwl} customStyle={customStyle} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} />
+        </div>
        
       </div>
     ) : (
