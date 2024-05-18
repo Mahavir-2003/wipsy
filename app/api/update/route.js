db00728116cc3141c0b1057ebc15386fb16bfdce
Mahavir-2003 <patelmahavir2003@outlook.com>
2024-05-18 20:34:54 +0530

    remove API folder

--- app/api/update/route.js
+++
@@ -1,2 +0,0 @@
-// update the chat history of the user along with delete the chats and it's uploaded files from uploadcare storage after 24 hours automatically
-// store the chat history locally on the vercel server
